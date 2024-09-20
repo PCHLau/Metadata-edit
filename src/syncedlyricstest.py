1c14c8e5669223497af04b584ded96ad9f3fc520
patrick.zk.lau <patrick.zk.lau@gmail.com>
2024-09-20 17:46:57 +0200

    failed version of trying to pick synced lyrics source by lyrics start time, solves lyrics start time, but does not solve desync later on

--- src/syncedlyricstest.py
+++ src/syncedlyricstest.py
@@ -5,9 +5,58 @@ def synlyr(track, artist):
 
     entry = track + ' ' + artist
 
-    lrc = syncedlyrics.search(f'{entry}')
+    sources = ['musixmatch', 'netease', 'lrclib']
+    lrc = None
 
-    lrc = lrc.split('\n')
+    length = 10000000
+    for i in sources:
+        # pick the source that starts the earliest
+        result = syncedlyrics.search(f'{entry}', providers=[f'{i}'])
+        # print(result)
+        try:
+            result = result.split('\n')
+            if lrc == None:
+                lrc = result
+        except AttributeError:
+            continue
+        counter = 0
+        if i == 'netease':
+            ending = False
+            endpoint = 0
+            for i in range(len(result)):
+                if ending == True:
+                    break
+                found = False
+                for j in result[i]:
+                    if found == True:
+                        if j == ' ':
+                            break
+                        else:
+                            ending = True
+                            endpoint=i
+                            break
+                    if j == ']':
+                        found = True
+            
+            result = result[endpoint:]
+            
+
+
+                    
+            # while result[counter][11] == ' ':
+            #     print('hi')
+            #     counter+=1
+            # result = result[counter:]
+        time = int(result[counter][7:9])*10 + int(result[counter][4:6])*1000 + int(result[counter][1:3])*1000*60
+        if time < length:
+            length = time
+            lrc = result
+
+    print(lrc)
+
+    # lrc = syncedlyrics.search(f'{entry}', providers=[true_source])
+
+    # lrc = lrc.split('\n')
 
     sdata = list()
     data = str()
@@ -16,13 +65,17 @@ def synlyr(track, artist):
     # data = str()
 
     for i in lrc:
-
+        if i =='':
+            continue
         try:
             time  = int(i[7:9])*10 + int(i[4:6])*1000 + int(i[1:3])*1000*60
             text = i[11:]
             sdata.append((text, time))
             data = data + text + '\n'
+
+        # account for different format, in this case unsynced lyrics with [verse] [chorus] etc
         except ValueError:
+            print('hi')
             sdata = None
             for i in lrc:
                 if i == '':
@@ -33,3 +86,5 @@ def synlyr(track, artist):
                     data = data + i + '\n'
 
     return sdata, data
+
+# print(synlyr('colors', 'flow'))
